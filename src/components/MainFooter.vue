8b937d184e4e794e298e8dd839d327dca5e29fe3
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-05 17:40:49 +0200

    add footer nav

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -24,7 +24,54 @@
                 </li>
             </ul>
         </div>
-        <div class="container"></div>
+        <div class="nav">
+            <div class="container">
+                <div>
+                    <h3>DC comics</h3>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Sites</h3>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div>
+                <img src="../assets/dc-logo-bg.png" alt="">
+            </div>
+        </div>
         <div class="container"></div>
     </footer>
 </template>
@@ -60,6 +107,28 @@ export default {
                 width: 60px;
                 aspect-ratio: 1 / 1;
                 object-fit: contain;
+            }
+        }
+    }
+
+    .nav {
+        padding: 3rem 0;
+        background: url('../assets/footer-bg.jpg') center/cover no-repeat #1c1c1c;
+        color: #fff;
+        overflow: hidden;
+
+        .container {
+            display: flex;
+            gap: 1.5em;
+            position: relative;
+
+            img {
+                width: 560px;
+                margin-left: auto;
+                position: absolute;
+                top: 50%;
+                right: 0;
+                transform: translateY(-50%);
             }
         }
     }
