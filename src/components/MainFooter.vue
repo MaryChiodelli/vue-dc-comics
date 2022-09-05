1fd4d2824ee4c981b351214330235a19d4f5e0ad
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-05 17:05:57 +0200

    add footer features

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>
-        Page footer
-    </div>
+    <footer>
+        <div class="features">
+            <ul class="container feature-list">
+                <li class="feature-item">
+                    <img src="../assets/buy-comics-digital-comics.png" alt="">
+                    Digital comics
+                </li>
+                <li class="feature-item">
+                    <img src="../assets/buy-comics-merchandise.png" alt="">
+                    DC merchandise
+                </li>
+                <li class="feature-item">
+                    <img src="../assets/buy-comics-subscriptions.png" alt="">
+                    Subscription
+                </li>
+                <li class="feature-item">
+                    <img src="../assets/buy-comics-shop-locator.png" alt="">
+                    Comic shop locator
+                </li>
+                <li class="feature-item">
+                    <img src="../assets/buy-dc-power-visa.svg" alt="">
+                    DC power visa
+                </li>
+            </ul>
+        </div>
+        <div class="container"></div>
+        <div class="container"></div>
+    </footer>
 </template>
 
 <script>
@@ -11,5 +36,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .features {
+        padding: 3rem 0;
+        background-color: #0282f9;
 
+        .feature-list {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .feature-item {
+            flex-basis: 0;
+            flex-grow: 1;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 12px;
+            color: #fff;
+            text-transform: uppercase;
+
+            img {
+                width: 60px;
+                aspect-ratio: 1 / 1;
+                object-fit: contain;
+            }
+        }
+    }
 </style>
