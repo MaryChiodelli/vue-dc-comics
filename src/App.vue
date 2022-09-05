5228d13e40ba8dcd622a72e29f36441ef335599b
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-05 15:25:39 +0200

    add base structure

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <MainHeader />
+    <MainContent />
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+import MainContent from './components/MainContent.vue';
+import MainFooter from './components/MainFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    MainContent,
+    MainFooter
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
