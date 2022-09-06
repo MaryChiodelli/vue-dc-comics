01dd57fb3ef5e2201d53a4b0156ec3b8f5087a09
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-06 15:23:17 +0200

    add card component

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,22 +1,37 @@
 <template>
     <main>
-        <div class="container">
-            Content goes here
+        <div class="hero">
+            <!-- image -->
+            <!-- Heading -->
+        </div>
+        <div class="series">
+            <div class="container">
+                <ProductCard />
+            </div>
         </div>
     </main>
 </template>
 
 <script>
+import ProductCard from './ProductCard.vue';
+
 export default {
-    
+    components: {
+        ProductCard
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     main {
-        padding: 3rem;
+        .hero {
+            height: 400px;
+            background: url('../assets/jumbotron.jpg') top/cover no-repeat;
+            background-color: #1c1c1c;
+        }
 
-        color: #fff;
-        background-color: #1c1c1c;
+        .series {
+            background-color: #1c1c1c;
+        }
     }
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+        <h3>Titolo</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
